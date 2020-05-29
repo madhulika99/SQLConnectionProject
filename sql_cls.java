7da90244edcb8a1e22f42979676553b753c9a2cf
singh@Kris-PC <singh@Kris-PC>
2020-05-29 16:50:39 -0500

    to show modification

--- sql_cls.java
+++
@@ -1,2 +0,0 @@
-//just to check whether the pull is not workng or yes
-//lets check this
---
+++ src/mysql/sql_cls.java
@@ -0,0 +1,4 @@
+//just to check whether the pull is not workng or yes
+//lets check this
+//git pull origin master is used for this
+// to push git push -u origin master
